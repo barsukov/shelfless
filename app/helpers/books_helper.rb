7ce4362501835061ca0a320fe8b5f744e2f6c3e5
Vladimir Barsukov <vladimir.barsukov@infopark.de>
2015-04-08 21:19:46 +0200

    book tick helper

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,12 @@
 module BooksHelper
+  def get_boolean_icon(value)
+    if value
+      content = "✓"
+      label = "label label-success"
+    else
+      content = "✘"
+      label = "label label-danger"
+    end
+    content_tag(:span, content, class: label)
+  end
 end
