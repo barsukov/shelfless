b41359006e96583ab137b6dc080c6e09c8be4242
Vladimir Barsukov <vladimir.barsukov@infopark.de>
2015-04-22 21:51:00 +0200

    finish editing settings after confirmaion

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -3,7 +3,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   private
 
   def after_confirmation_path_for(resource_name, resource)
-    account_path
+    account_path(resource.account.try(:id))
   end
 
 end
