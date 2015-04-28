864cf58f351fd697246e0d7463bf9c8add6844eb
Vladimir Barsukov <vladimir.barsukov@infopark.de>
2015-04-28 21:52:19 +0200

    sidekiq for redis

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+redis_conn = proc {
+  Redis.current
+}
+Sidekiq.configure_client do |config|
+  config.redis = ConnectionPool.new(size: 5, &redis_conn)
+end
+Sidekiq.configure_server do |config|
+  config.redis = ConnectionPool.new(size: 25, &redis_conn)
+end
