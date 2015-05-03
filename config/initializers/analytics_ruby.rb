870a0f3b7cdba953981c3a7c171f880616930057
Vladimir Barsukov <vladimir.barsukov@infopark.de>
2015-05-03 21:30:43 +0200

    analytics integration

--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,6 +1,6 @@
 require 'segment/analytics'
 
 Analytics = Segment::Analytics.new({
-    write_key: 'G1ahMGKtNgyUA2infc4cFC6DI0P8lpPc',
+    write_key: ENV["SEGMENT_IO_WRITE_KEY"],
     on_error: Proc.new { |status, msg| print msg }
 })
