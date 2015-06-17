71d058797e84d064c2ccdae38bd8eddc9ebe1e64
Vladimir Barsukov <vladimir.barsukov@infopark.de>
2015-06-17 09:31:42 +0200

    fix helper and sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,10 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    sign_in_url = new_user_session_url
-    if request.referer == sign_in_url
-      super
-    else
-      stored_location_for(resource) || request.referer || books_path
-    end
+    stored_location_for(resource) || books_path
+  end
+
+  def after_sign_out_path_for(resource)
+    stored_location_for(resource) || books_path
   end
 end
