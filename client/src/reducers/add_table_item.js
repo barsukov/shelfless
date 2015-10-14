30e5adf1c50f4892dc044892c115b9a27d46b055
Vladimir Barsukov <vladimir.barsukov@infopark.de>
2015-10-14 21:05:58 +0200

    removed debugger

--- client/src/reducers/add_table_item.js
+++ client/src/reducers/add_table_item.js
@@ -1,8 +1,6 @@
 var addTableItemAction = require('../actions/add_table_item')
-debugger
 
 export function addTableItem(state, action) {
-  debugger
   switch (action.type) {
     case addTableItemAction.ADD_TABLE_ITEM:
       return Object.assign({}, state, {
