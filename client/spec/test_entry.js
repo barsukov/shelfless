dd99d84e8d6e1c1cea3a9ba92e67114f9ea888ea
Vladimir Barsukov <pro.barsukov@gmail.com>
2015-12-18 23:45:30 +0100

    refactored mocha config and fix the author bug

--- client/spec/test_entry.js
+++ client/spec/test_entry.js
@@ -1,2 +1,5 @@
-var testsContext = require.context(".", true, /_spec$/);
-testsContext.keys().forEach(testsContext);
+require("babel-polyfill");
+var code = require.context('.', true, /\_spec.js$/);
+var copmonents = require.context('.', true, /\.jsx$/);
+code.keys().forEach(code);
+copmonents.keys().forEach(copmonents);
--- client/webpack.mocha.config.js
+++ client/webpack.mocha.config.js
@@ -1,41 +1,23 @@
 var nodeDir = __dirname + '/node_modules';
 var webpack = require('webpack')
 var RewirePlugin = require("rewire-webpack");
-module.exports = {
-  context: __dirname,
-  output: 'spec/dist/s.js',
-  entry: "./spec/test_entry",
-  resolve: {
-    alias: {
-      'react': nodeDir +'/react/',
-      'underscore': nodeDir +'/underscore/underscore-min.js',
-      'jquery': nodeDir +'/jquery/dist/jquery.js'
-    },
-    extensions: ["", ".jsx", ".cjsx", ".coffee", ".js"],
-    modulesDirectories: ["js", "node_modules"]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new RewirePlugin(),
-    new webpack.ProvidePlugin({
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch',
-      "_": "underscore",
-      "React": 'react',
-      "$": "jquery",
-      "moment": "moment"
-    })
-  ],
-
-  module: {
-    noParse: [
-       /node_modules\/sinon/,
-    ],
-    loaders: [
-        {test: /\.css$/, loader: 'style-loader!css-loader'},
-        {test: /\.jsx$/, exclude: /node_modules/, loader: 'babel' },
-        {test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
-        {test: require.resolve('babel-polyfill'), loader: 'babel'}
-    ]
-  }
+const config = require('./webpack.common.config');
+var webpack = require('webpack')
+config.output = {
+  filename: 'bundle.js',
+  path: 'spec/dist'
 };
+// You can add entry points specific to rails here
+config.entry.push('./spec/test_entry');
+config.plugins.push(new webpack.ProvidePlugin({
+  'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch',
+  "React": 'react',
+  "$": 'jquery'
+}))
+config.module.noParse.push(/node_modules\/sinon/);
+
+config.module.loaders.push(
+  {test: /\.jsx$/, exclude: /node_modules/, loader: 'babel' },
+  {test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+);
+module.exports = config;
