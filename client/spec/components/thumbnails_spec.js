2e077dde5fdc5338bb79e7bb066e5f8ea05dff2f
Vladimir Barsukov <pro.barsukov@gmail.com>
2016-01-05 09:31:18 +0100

    fixed mock tests:

--- client/spec/components/thumbnails_spec.js
+++ client/spec/components/thumbnails_spec.js
@@ -3,12 +3,15 @@ import Ract from 'react'
 import ReactTestUtils from 'react-addons-test-utils'
 import Thumbnails from '../../src/components/thumbnails'
 import { expect } from 'chai'
+import sinon from 'sinon/pkg/sinon';
 
 describe('Thumbnails', function () {
   var component;
+  var loadAdditional;
 
   beforeEach(function () {
-    component = ReactTestUtils.renderIntoDocument(<Thumbnails books={[]} />);
+    loadAdditional = sinon.spy()
+    component = ReactTestUtils.renderIntoDocument(<Thumbnails loadAdditional={loadAdditional} books={[]} />);
   });
 
   it('should create a new instance of Thumbnails', function () {
