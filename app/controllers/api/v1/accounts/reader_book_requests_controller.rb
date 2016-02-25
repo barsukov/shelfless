3bc2292e9142c833a9b1973344b1ae03e3e7ffdc
Vladimir Barsukov <pro.barsukov@gmail.com>
2016-02-25 10:44:53 +0100

    fixed asap remove dead code

--- app/controllers/api/v1/accounts/reader_book_requests_controller.rb
+++ app/controllers/api/v1/accounts/reader_book_requests_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Accounts::ReaderBookRequestsController < ::SinglePageApplicationController
+class Api::V1::Accounts::ReaderBookRequestsController < ApplicationController
   include AccountKeeper
   skip_before_filter :verify_authenticity_token, only: :create
   # POST /books
