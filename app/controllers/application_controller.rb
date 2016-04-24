0c458a24c5b7940bd1355876dc177acd03aa4b01
Vladimir Barsukov <pro.barsukov@gmail.com>
2016-04-24 21:55:02 +0200

    new main integration is back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || books_path
+    stored_location_for(resource) || url_for("/single_page_application/books")
   end
 
   def after_sign_out_path_for(resource)
-    stored_location_for(resource) || books_path
+    stored_location_for(resource) || root_path
   end
 end
